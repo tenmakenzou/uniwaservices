6aa1f0e4891577f7371d4503d6217227c1bf3f93
tenmakenzou <generous35@yahoo.com>
2024-10-07 18:23:14 +0300

    gui and new additions

---
+++ library.py
@@ -0,0 +1,38 @@
+from datetime import datetime
+
+def get_library():
+    current_date = datetime.now()
+    day_of_week = current_date.weekday()  
+    days_of_week = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+    x = days_of_week[day_of_week]
+    current_hour = current_date.hour 
+    
+    current_time = float(current_date.strftime("%H.%M"))
+    if (9.00 <= current_time <= 19.00) and (x != 'Friday') and (x != 'Saturday') and (x != 'Sunday'):
+        return "Η Βιβλιοθήκη είναι ανοιχτή\nLibrary is open"
+    elif (9.00 <= current_time <= 15.30) and (x == 'Friday') and (x != 'Saturday') and (x != 'Sunday'):
+        return "Η Βιβλιοθήκη είναι ανοιχτή\nLibrary is open"
+    else:
+        return "Η Βιβλιοθήκη είναι κλειστή\n|Library is closed"
+
+
+def get_restauraunt():
+        current_date = datetime.now()
+        current_hour = current_date.hour 
+        current_time = float(current_date.strftime("%H.%M"))
+
+        if ((7.30 <= current_time <= 9.0) or (12.30 <= current_time <= 15.40) or (18.00 <= current_time <= 20.30)):
+            return("Η σιτίση ειναι ανοιχτή\nRestauraunt is open")
+            
+        else:
+            if current_time < 7.30 or current_time >20.30:
+                next_opening_time = "7:30"
+                return (f"Η σιτίση είναι κλείστη , ανοίγει στις [{next_opening_time}]\nRestauraunt is closed and opens at [{next_opening_time}]")
+            elif current_time < 12.30:
+                next_opening_time = "12:30"
+                return (f"Η σιτίση είναι κλείστη , ανοίγει στις [{next_opening_time}]\nRestauraunt is closed and opens at [{next_opening_time}]")
+
+            elif current_time < 18.00:
+                next_opening_time = "18:00"
+                return (f"Η σιτίση είναι κλείστη , ανοίγει στις [{next_opening_time}]\nRestauraunt is closed and opens at [{next_opening_time}]")
+
--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+#gui related
 from kivy.app import App 
 from kivy.uix.label import Label 
 from kivy.uix.boxlayout import BoxLayout
@@ -5,7 +6,15 @@ from kivy.uix.button import Button
 from kivy.uix.anchorlayout import AnchorLayout
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.widget import Widget
+
+
+import webbrowser
+
+
+
+#python script related
 from sitish import *
+from library import * 
 
 class MyLayout(BoxLayout):
     def __init__(self):
@@ -13,18 +22,25 @@ class MyLayout(BoxLayout):
         self.anchor_layout = AnchorLayout()
         self.buttons_layout = BoxLayout(orientation='vertical', size_hint=(None, None))
 
-        self.button = Button(text="Menu sitishs", size_hint=(None, None), size=(200, 50))
-        self.button2 = Button(text="Services", size_hint=(None, None), size=(200, 50))
+        self.button = Button(text="Restauraunt Menu\nΠρογραμμα Σίτισης", size_hint=(None, None), size=(200, 50))
+        self.button2 = Button(text="Library / Restauraunt opening\nΒιβλιοθήκη / Λέσχη ωράριο ", size_hint=(None, None), size=(200, 50))
 
-        self.button.bind(on_press=self.new_label)
+        self.button3 = Button(text="Services ", size_hint=(None, None), size=(200, 50))
+
+
+
+        self.button.bind(on_press=self.sitish)
+        self.button2.bind(on_press=self.schedule)
+        self.button3.bind(on_press=self.services)
         
         self.buttons_layout.add_widget(self.button)
         self.buttons_layout.add_widget(self.button2)
+        self.buttons_layout.add_widget(self.button3)
 
         self.anchor_layout.add_widget(self.buttons_layout)
         self.add_widget(self.anchor_layout)
 
-    def new_label(self, button):
+    def sitish(self, button):
         self.anchor_layout.clear_widgets()
         self.grid_layout = GridLayout(cols=1, spacing=0, size_hint=(1, 1))
         self.label = Label(text=run(), text_size=(None, None), halign='center', valign='middle', size_hint=(1, 0.9))
@@ -39,11 +55,62 @@ class MyLayout(BoxLayout):
         self.grid_layout.add_widget(back_button_anchor)
         self.anchor_layout.add_widget(self.grid_layout)
 
+
+
+    def schedule(self, button):
+        self.anchor_layout.clear_widgets()
+        self.grid_layout = GridLayout(cols=1, spacing=0, size_hint=(1, 1))
+        
+        self.label = Label(text=get_library(), text_size=(None, None), halign='center', valign='middle', size_hint=(1, 0.5))
+        self.label.bind(size=self.label.setter('text_size'))
+        self.label2 = Label(text=get_restauraunt(), text_size=(None, None), halign='center', valign='middle', size_hint=(1, 0.5))
+        self.label2.bind(size=self.label.setter('text_size'))
+        
+        self.back_button = Button(text="Back", size_hint=(None, None), size=(200, 50))
+        self.back_button.bind(on_press=self.reset_screen)
+
+        back_button_anchor = AnchorLayout(anchor_x='center', anchor_y='center', size_hint=(1, 0.1))
+        back_button_anchor.add_widget(self.back_button)
+
+        self.grid_layout.add_widget(self.label)
+        self.grid_layout.add_widget(self.label2)
+        self.grid_layout.add_widget(back_button_anchor)
+        self.anchor_layout.add_widget(self.grid_layout)
+    
+    
+    def services(self, button):
+        self.anchor_layout.clear_widgets()
+        self.grid_layout = GridLayout(cols=1, spacing=10, size_hint=(1, 1))
+
+        eclass_button = Button(text="Eclass", size_hint=(None, None), size=(200, 50))
+        eclass_button.bind(on_press=lambda x: webbrowser.open("https://eclass.uniwa.gr/"))
+
+        services_button = Button(text="Services", size_hint=(None, None), size=(200, 50))
+        services_button.bind(on_press=lambda x: webbrowser.open("https://sso.uniwa.gr/login?service=https%3A%2F%2Fservices.uniwa.gr%2Flogin%2Fcas"))
+
+        self.back_button = Button(text="Back", size_hint=(None, None), size=(200, 50))
+        self.back_button.bind(on_press=self.reset_screen)
+
+        buttons_anchor = AnchorLayout(anchor_x='center', anchor_y='center')
+        buttons_anchor.add_widget(self.grid_layout)
+
+        self.grid_layout.add_widget(eclass_button)
+        self.grid_layout.add_widget(services_button)
+
+        back_button_anchor = AnchorLayout(anchor_x='center', anchor_y='center', size_hint=(1, 0.1))
+        back_button_anchor.add_widget(self.back_button)
+        
+        
+        self.anchor_layout.add_widget(buttons_anchor)
+        self.anchor_layout.add_widget(back_button_anchor)
+
+
     def reset_screen(self, button):
         self.anchor_layout.clear_widgets()
         self.buttons_layout.clear_widgets()
         self.buttons_layout.add_widget(self.button)
         self.buttons_layout.add_widget(self.button2)
+        self.buttons_layout.add_widget(self.button3)    
         self.anchor_layout.add_widget(self.buttons_layout)
 
 class Services(App):
